52518374b8b038506f41c27a85e3ce041194fc63
Liz Manresa <manresa.liz@gmail.com>
2023-06-13 22:17:11 +0200

    list done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace S04_list;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> stringCollection = new List<string>();
+
+        stringCollection.Add("Hello");
+        stringCollection.Add("World");
+        stringCollection.Add("Paradise");
+
+        stringCollection.RemoveAt(0);
+
+        stringCollection.Remove("Paradise");
+
+        stringCollection[0] = "Heaven";
+
+        foreach (string element in stringCollection)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
